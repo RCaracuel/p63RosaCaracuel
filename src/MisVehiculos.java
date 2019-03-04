3d15d03d486f4dc379b6f51882ab197d229678a9
RCaracuel <rosa.caracuel@gmail.com>
2019-03-04 10:33:16 +0100

    Implementación de métodos en el main

--- src/MisVehiculos.java
+++ src/MisVehiculos.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,8 @@
  */
 public class MisVehiculos {
     public static void main(String[] args) {
+        
+        Scanner teclado=new Scanner(System.in);
          EmpresaAlquilerVehiculos prueba= new EmpresaAlquilerVehiculos("iouiuhi","Rosa","wwww.iowjfjhweof.com");
         
 
@@ -30,5 +35,44 @@ public class MisVehiculos {
         System.out.println("ORDENADOS LOS CLIENTES");
         prueba.imprimirClientes();
       
+        System.out.println("-----------------------");
+        prueba.burbujaVehiculos();
+        System.out.println("ORDENADOR POR MATRÍCULA");
+        prueba.imprimirVehiculos();
+        System.out.println("-----------------------");
+        System.out.println("Introduzca Nif a buscar:");
+        String aux=teclado.nextLine();
+        
+        prueba.busquedaNif(aux);
+        
+        if(prueba.busquedaNif(aux)==-1){
+            System.out.println("No se ha encontrado");
+        }else{
+            System.out.println("Lo hemos encontrado en la posición "+prueba.busquedaNif(aux));
+        }
+        
+        System.out.println("------------------------");
+        System.out.println("Introduzca matrícula a buscar");
+        String aux2=teclado.nextLine();
+        
+        prueba.busquedaMatricula(aux2);
+        
+        if(prueba.busquedaMatricula(aux2)==-1){
+            System.out.println("No se ha encontrado");
+        }else{
+            System.out.println("Lo hemos encontrado en la posición "+prueba.busquedaMatricula(aux2));
+        }
+        
+        System.out.println("Introduzca mátricula para alquilar:");
+        String matAlquiler=teclado.nextLine();
+        System.out.println("Introduzca nif:");
+        String nifAlquiler=teclado.nextLine();
+        System.out.println("Introduzca número de días:");
+        int dias=teclado.nextInt();
+        
+        teclado.nextLine();
+        
+        prueba.alquilarVehiculo(matAlquiler, nifAlquiler, dias);
+        
     }
 }
